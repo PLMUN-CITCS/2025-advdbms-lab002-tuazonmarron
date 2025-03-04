c6bbcf78d501f27689c6ed0c00cfa5d0c968f5ee
tuazonmarron <tuazonmarron_bsit@plmun.edu.ph>
2025-03-04 14:34:24 +0800

    Completed assignment: Created tables and inserted data

--- scripts/01_create_tables.sql
+++ scripts/01_create_tables.sql
@@ -9,4 +9,10 @@
         - student_phone (VARCHAR(50))
 
     2. Place your create table query below this comment
-*/
+*/ 
+CREATE TABLE tblStudents (
+	student_id INT PRIMARY KEY AUTO_INCREMENT,
+	student_name VARCHAR(50),
+	student_email VARCHAR(50),
+	student_phone VARCHAR(50)
+) ;
--- scripts/02_insert_data.sql
+++ scripts/02_insert_data.sql
@@ -12,4 +12,9 @@
         - student_phone: 123-456-7890
 
     2. Place your insert queries below this comment
-*/
+*/ 
+INSERT INTO
+	tblStudents (student_name, student_email, student_phone)
+	VALUES
+	("John Doe", "johndoe@gmail.com", "123-456-7890"),
+	("Jane Doe", "janedoe@gmail.com", "123-456-7890");
